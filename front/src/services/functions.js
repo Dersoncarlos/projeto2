15ed0b1621fdde69dd77873cf3b1077dc12bc3de
flaviolima9904 <flaviolima9904@gmail.com>
2022-04-09 19:23:09 -0300

    exemplos

--- front/src/services/functions.js
+++ front/src/services/functions.js
@@ -1,46 +1,46 @@
-import api from './api';
-
+import api from "./api";
 
 export function uploadFile(file) {
-    try {
-        apiUploadCsv(file)
-
-    } catch (error) {
-        console.log('error ' + error);
-    }
+  try {
+    apiUploadCsv(file);
+  } catch (error) {
+    console.log("error " + error);
+  }
 }
 
+const apiUploadCsv = async (payload) => {
+  console.log("payload", payload);
 
-const apiUploadCsv = async payload => {
-    console.log('payload', payload);
+  const dataForm = new FormData();
+  dataForm.append("csv", {
+    file: payload.csv,
+    name:"VEIFICA SE VEM NO 'payload.csv'",
+    type:"VEIFICA SE VEM NO 'payload.csv'"
 
-    const dataForm = new FormData();
-    dataForm.append('csv', `${payload.csv}`);
+  });
 
-    const request = await api.post('api/csv/upload', dataForm)
+  const request = await api.post("api/csv/upload", dataForm, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 
-
-    return request
-}
+  return request;
+};
 
 export function testando(file) {
-    try {
-        testApi(file)
-
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    testApi(file);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const testApi = async payload => {
-    console.log('get')
-    try {
-        const fields = payload;
-        const request = await api.get('')
-        console.log('request back', request)
-    } catch (err) {
-        console.log('deu ruim', +err)
-    }
-
-}
-
+const testApi = async (payload) => {
+  console.log("get");
+  try {
+    const fields = payload;
+    const request = await api.get("");
+    console.log("request back", request);
+  } catch (err) {
+    console.log("deu ruim", +err);
+  }
+};
